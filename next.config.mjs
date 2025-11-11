9dd834e184b62f0c6e2859f9df5d9a42dda00d85
Tanu Yadav <yadavtanu882@gmail.com>
2025-11-11 22:07:31 +0530

    push my project

---
+++ next.config.mjs
@@ -0,0 +1,28 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    serverComponentsHmrCache: false,
+    serverActions: {
+      bodySizeLimit: '10mb', // ✅ Increase limit to 10 MB
+    },
+  },
+
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "unynhpehrxfgkuwcxpmg.supabase.co"
+      }
+    ]
+  },
+  
+   webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
+};
+
+export default nextConfig;
